3c5e915da192283d1a3d0e44555a54173a69dcd8
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-20 10:02:42 +0200

    Update

--- FundMe.sol
+++ FundMe.sol
@@ -24,12 +24,12 @@ contract FundMe {
     }
 
     function getAggregatorVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); // rinkeby eth to usd conversion address from chainlink data feeds
         return priceFeed.version();
     }
 
     function getEthPriceInWei() public view returns (uint256) { 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); // rinkeby eth to usd conversion address from chainlink data feeds
         (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer) * 10000000000;
     }
