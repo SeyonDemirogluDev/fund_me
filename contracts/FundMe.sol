9e9182307486fd9c28ed8015821023bc8defdfba
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-15 15:32:40 +0200

    Reduce code size

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,7 +10,7 @@ contract FundMe {
     
     address[] public funders;
 
-    mapping(address => uint256) public fundedAmount;
+    mapping (address => uint256) public fundedAmount;
 
     constructor() {
         owner = msg.sender;
@@ -26,7 +26,7 @@ contract FundMe {
      * @dev Remote function
      */
     function _getAggregatorVersion() private view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); // rinkeby address from chainlink data feeds
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
     }
 
@@ -34,7 +34,7 @@ contract FundMe {
      * @dev Required for _getConversionRate()
      */
     function _getEthPriceInWei() private view returns (uint256) { 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); // rinkeby address from chainlink data feeds
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer) * 1e10;
     }
